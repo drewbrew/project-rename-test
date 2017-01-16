abd19ac18843b10d8b02d7a89e070802f735bd23
Drew Winstel <drewbrew@users.noreply.github.com>
2017-01-16 13:48:40 -0600

    BROKEN STATE: rename model Blob to Blargh

--- testapp/test2/models.py
+++ testapp/test2/models.py
@@ -6,7 +6,7 @@ from simple_history import register
 # Create your models here.
 class Other(models.Model):
     blob = models.ForeignKey(
-        'testapp.blob', models.DO_NOTHING, related_name='others')
+        'testapp.blargh', models.DO_NOTHING, related_name='others')
     name = models.CharField(max_length=20, unique=True)
 
 
--- testapp/testapp/models.py
+++ testapp/testapp/models.py
@@ -5,13 +5,13 @@ from django.db import models
 from simple_history import register
 
 
-class Blob(models.Model):
+class Blargh(models.Model):
     name = models.CharField(unique=True, max_length=50)
 
 
 class Dummy(models.Model):
-    blob = models.ForeignKey('blob', models.CASCADE, related_name='dummies')
+    blob = models.ForeignKey('blargh', models.CASCADE, related_name='dummies')
     name = models.CharField(max_length=25, unique=True)
 
-for model in [Blob, Dummy]:
+for model in [Blargh, Dummy]:
     register(model)
